281c4d32b1828d3f200279cc0ccf30c5f66becd7
sandipan-das-sd <sd901656@gmail.com>
2025-12-21 13:06:10 +0530

    feat: Remove unused eas projectId configuration from app.config.js

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -65,10 +65,7 @@ export default {
     extra: {
       API_URL: process.env.API_URL || "http://192.168.31.102:6000/api/v1",
       WS_URL: process.env.WS_URL || "http://192.168.31.102:6000",
-      GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-      eas: {
-        projectId: "c4a1d50a-2bae-4cb0-a413-6e98a5b9eafa"
-      }
+      GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY
     }
   }
 };
